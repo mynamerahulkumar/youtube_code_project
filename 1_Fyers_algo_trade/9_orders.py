7afa850e786a25e3e095117166b94b9ec9d4d6d6
Rahul Kumar <mynamerahulkumar13@gmail.com>
2025-11-12 08:45:30 +0530

    updated code

--- 1_Fyers_algo_trade/9_orders.py
+++ 1_Fyers_algo_trade/9_orders.py
@@ -1,5 +1,3 @@
-
-
 import datetime
 from fyers_apiv3 import fyersModel
 
@@ -7,30 +5,28 @@ from fyers_apiv3 import fyersModel
 client_id = open("client_id.txt",'r').read()
 access_token = open("access_token.txt",'r').read()
 
-# Initialize the FyersModel instance with your client_id, access_token, and enable async mode
-fyers = fyersModel.FyersModel(client_id=client_id, is_async=False, token=access_token, log_path="")
+fyers=fyersModel.FyersModel(client_id=client_id,is_async=False,token=access_token,log_path="/Users/rahulkumar/Documents/GitHub/youtube_code_project/1_Fyers_algo_trade/logs")
 
-def placeOrder(inst ,t_type,qty,order_type,price=0, price_stop=0):
-    exch = inst[:3]
-    symb = inst[4:]
-    dt = datetime.datetime.now()
+# NSE-RIL
+def placeOrder(inst,t_type,qty,order_type,price=0,price_stop=0):
+    exch=inst[:3]
+    symb=inst[4:]
+    dt=datetime.datatime.now()
     print(dt.hour,":",dt.minute,":",dt.second ," => ",t_type," ",symb," ",qty," ",order_type," @ price =  ",price)
     if(order_type=="MARKET"):
-        type1 = 2
-        price = 0
-        price_stop = 0
+        type1=2
+        price=0
+        price_stop=0
     elif(order_type=="LIMIT"):
-        type1 = 1
-        price_stop = 0
+        type1=1
+        price_stop=0
     elif(order_type=="SL-LIMIT"):
-        type1 = 4
-
+        type1=4
     if(t_type=="BUY"):
         side1=1
     elif(t_type=="SELL"):
         side1=-1
-
-    data =  {
+    data={
         "symbol":inst,
         "qty":qty,
         "type":type1,
@@ -38,23 +34,20 @@ def placeOrder(inst ,t_type,qty,order_type,price=0, price_stop=0):
         "productType":"INTRADAY",
         "limitPrice":price,
         "stopPrice":price_stop,
-        "validity":"DAY",
+        "validity":"DAY"
     }
-
     try:
-        orderid = fyers.place_order(data)
+        orderId=fyers.place_order(data)
         print(dt.hour,":",dt.minute,":",dt.second ," => ", symb , orderid)
-        return orderid
+        return orderId
     except Exception as e:
         print(dt.hour,":",dt.minute,":",dt.second ," => ", symb , "Failed : {} ".format(e))
-
-
+    
 def cancelOrder(order_id):
     #convert id into dictionary
     order_id_dict = {"id": str(order_id)}
     response = fyers.cancel_order(order_id_dict)
     print(response)
-
 
 def modifyOrder(orderId,order_type,qty,price=0, price_stop=0):
     if(order_type=="MARKET"):
@@ -78,10 +71,3 @@ def modifyOrder(orderId,order_type,qty,price=0, price_stop=0):
     print(response)
 
 
-
-#placeOrder("NSE:SBIN-EQ" ,'SELL',1,"MARKET")
-#placeOrder("NSE:SBIN-EQ" ,'BUY',1,"LIMIT",550)
-#placeOrder("NSE:SBIN-EQ" ,'SELL',1,"SL-LIMIT",579,580)
-
-cancelOrder(23091300358683)
-#modifyOrder(230913003523,"MARKET",1)
---
+++ 1_Fyers_algo_trade/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 1-fyers-algo-trade!")
+
+
+if __name__ == "__main__":
+    main()
